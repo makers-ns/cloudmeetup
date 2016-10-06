6bbcbb9f15df9750033ad787aaac13f83eaadb4a
Nenad Cetic <nenad.cetic@gmail.com>
2016-10-06 17:54:46 +0200

    Adding mini example for AWS
    More info at
    https://docs.aws.amazon.com/iot/latest/developerguide/iot-device-sdk-node.html

---
+++ aws/app.js
@@ -0,0 +1,34 @@
+var awsIot = require('aws-iot-device-sdk');
+
+var device = awsIot.device({
+	"host": "a2qafgaoqmsxfi.iot.eu-west-1.amazonaws.com",
+	"port": 8883,
+	"clientId": "myPCasAThing",
+	"thingName": "myPCasAThing",
+	"caCert": "root-CA.crt",
+	"clientCert": "e85ef9132a-certificate.pem.crt", // change this
+	"privateKey": "e85ef9132a-private.pem.key" // change this
+});
+//
+// Device is an instance returned by mqtt.Client(), see mqtt.js for full
+// documentation.
+//
+device
+ .on('connect', function() {
+   console.log('connect');
+   device.subscribe('control');
+   setInterval(function () {
+      var TOPIC = 'iot-2/evt/status/fmt/json';
+        device.publish('rms', '{"RMS":' + random(-96, 0) + '}');//Payload
+   }, 2000); //Publishing every 2000ms
+
+   });
+
+device
+ .on('message', function(topic, payload) {
+   console.log('message', topic, payload.toString());
+ });
+
+function random (low, high) {
+    return Math.random() * (high - low) + low;
+}
